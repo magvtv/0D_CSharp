01d2940c881e0353096236915bf318cbf193a415
magvtv <magutujr@gmail.com>
2024-05-14 17:41:52 +0300

    managed to get more with microsoft learn

--- Program.cs
+++ Program.cs
@@ -1,128 +1,72 @@
 ﻿using System;
-using System.Linq;
-using System.Text;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using System.IO;
 
-namespace Learning
+
+class Program
 {
-    // class Smartphone
-    // {
-    //     public string Brand { get; set; }
-    //     public string Model { get; set; }
-    //     public decimal Price { get; set; }
-
-    //     // constructor to init the field properties
-    //     public Smartphone(string brand, string model, decimal price)
-    //     {
-    //             Brand = brand;
-    //             Model = model;
-    //             Price = price;
-    //     }
-
-    //     public void phoneSummary()
-    //     {
-    //         return $"Phone: {brand}\nModel: {model}\nPrice in KES: {price}";
-    //     }
-    // }
-
-    // class Program
-    // {
-    //     public static void Main(string[] args)
-    //     {
-    //         Console.Write("Enter brand: ");
-    //         string brand = Console.ReadLine();
-
-    //         Console.Write("Enter model: ");
-    //         string model = Console.ReadLine();
-
-    //         Console.Write("Enter price (KES): ");
-    //         decimal price;
-    //         while (!decimal.TryParse(Console.ReadLine(), out price) || price < 0)
-    //         {
-    //             Console.WriteLine("Price should not be less than zero!");
-    //             Console.Write("Enter price (KES): ");
-    //         }
-
-
-
-    //         Smartphone smartphone = new Smartphone(brand, model, price);
-    //         string filePath = "Data/phones.txt";
-
-    //         using (StreamWriter writer = File.AppendText(filePath))
-    //         {
-    //             writer.WriteLine(smartphone.ToString());
-    //         }
-
-    //         smartphone.phoneSummary();
-    //         Console.WriteLine("Device saved to data folder!");
-    //     }
-    // }
-
-    // class Dog
-    // {
-    //     public void dogSummary(string dogName, string breed, int age)
-    //     {
-    //         Console.WriteLine($"Dog Details\nName: {dogName} \nBreed: {breed}\nAge: {age}");
-    //     }
-    // }
-
-    class Program
+    public static void ShowMedicineSchedule()
     {
-        public static void Main(string[] args)
+        /* Format and display medicine times */
+        int[] times = {800, 1200, 1600, 2000}; 
+
+        foreach (int val in times)
         {
+            string time = val.ToString();
+            int len = time.Length;
 
-            // DATA STRUCTURES
-            // List<string> employees = new List<string> () {
-            //     "Dwayne",
-            //     "Tasha",
-            //     "Yogira",
-            //     "Laorea",
-            // };
-            // employees.Add("Priscilla");
-            // Console.WriteLine(employees[4]);
-
-
-            // Dictionary<string, int> cityPopulation = new Dictionary<string, int>();
-            // cityPopulation.Add("Tokyo", 37435191);
-            // cityPopulation.Add("Delhi", 29399141);
-            // cityPopulation.Add("Shanghai", 26317104);
-            // cityPopulation.Add("Mexico City", 21671908);
-            // cityPopulation.Add("Cairo", 20283552);
-
-            // for (int x = 0; x < cityPopulation.Count; x++)
-            // {
-            //     Console.WriteLine("{0}: Population of {1}", cityPopulation.ElementAt(x).Key, cityPopulation.ElementAt(x).Value.ToString("N0"));
-
-            // }
-            // string readText = File.ReadAllText("spotify.txt");
-            // Console.WriteLine(readText);
-            // File.Delete("spotify.txt");
-            // int[] numList = { 32, 43, 5, 3, 93, 85, 12 };
-            // int maxNum, minNum; 
-            // maxNum = numList[0];
-            // minNum = numList[0];
-            // for (int x = 0; x < numList.Length; x++)
-            // {
-            //     if (numList[x] > maxNum)
-            //     {
-            //         maxNum = numList[x];
-            //     }
-            //     else if (numList[x] < minNum)
-            //     {
-            //         minNum = numList[x];
-            //     }
-            // }
-            // Console.WriteLine($"Largest number is {maxNum} the smallest being {minNum}");
-            // int[][] weirdArray = new int[2][];
-            // weirdArray[0] = new int[] { 1, 3, 5 };
-            // weirdArray[1] = new int[] { 2, 4, 6 };
-            // Console.WriteLine(weirdArray[1][1]);
-
-            
-            
+            if (len >= 3)
+            {
+                time = time.Insert((len - 2), ":");
+            }
+            else if (len == 2)
+            {
+                time = time.Insert(0, "0:");
+            }
+            else
+            {
+                time = time.Insert(0, "0:0");
+            }
+            Console.Write($"{time}");
         }
     }
-    
+
+    public static void AdjustMedicineSchedule()
+    {
+        int[] times = {800, 1200, 1600, 2000}; 
+        int diff = 0;
+
+        for(int x = 0; x < times.Length; x++)
+        {
+            times[x] = (times[x] + diff) % 2400;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        int[] times = {800, 1200, 1600, 2000};
+        int diff = 0;
+
+        Console.WriteLine("Enter current GMT");
+        int currentGMT = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Current Medicine Schedule:");
+        ShowMedicineSchedule();
+
+        Console.WriteLine("Enter new GMT");
+        int newGMT = Convert.ToInt32(Console.ReadLine());
+
+        if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
+        {
+            Console.WriteLine("Invalid GMT");
+        }
+        else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0) 
+        {
+            diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
+            AdjustMedicineSchedule();
+        } 
+        else 
+        {
+            diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
+            AdjustMedicineSchedule();
+        }
+    }
 }
