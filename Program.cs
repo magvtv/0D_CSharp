488759fde7626f90422eaef615fbebb2a8b6b4df
magvtv <magutujr@gmail.com>
2024-05-20 09:41:37 +0300

    working on a renaissanc cli project
    working on a renaissance cli project

--- Program.cs
+++ Program.cs
@@ -4,86 +4,126 @@ using System.Collections.Generic;
 using System.Reflection;
 
 
-class Artwork
+
+namespace Renaissance
 {
-    public string Title { get; set; }
-    public string Summary { get; set; }
-}
-
-class Artist
-{
-    public string Name { get; set; }
-    public Dictionary <string, Artwork> Artworks { get; set; }
-    public Artist(string name)
+    class Artwork
     {
-        Name = name;
-        Artworks = new Dictionary<string, Artwork>();
+        public string Title { get; set; }
+        public string Summary { get; set; }
     }
 
-    public void AddArtwork (string title, string summary)
+    class Artist
     {
-        Artworks[title] = new Artwork { Title = title, Summary = summary };
-    }
-}
-
-
-class Renaissance
-{
-    private Dictionary<string, Artist> artists;
-    public Renaissance()
-    {
-        artists = new Dictionary<string, Artist> ();
-    }
-
-    public void AddArtist()
-    {
-        
-    }
-
-    public void AddArtwork()
-    {
-
-    }
-    public void ShowGallery()
-    {
-        Console.WriteLine("My Renaissance Art Gallery");
-
-    }
-}
-class Program
-{
-    
-    public static void Main(string[] args)
-    {
-
-        string[] artists = {
-            "Leonardo Da Vinci",
-            "Michelangelo",
-            "Raphael",
-            "Donatello",
-            "Titian",
-            "Sandro Botticelli",
-            "El Greco",
-            "Caravaggio",
-            "Hieronymus Bosch",
-        };
-
-        Queue <int> artworksTotal = new Queue<int>();
-        artworksTotal.Enqueue(20);
-        artworksTotal.Enqueue(40);
-        artworksTotal.Enqueue(60);
-        artworksTotal.Enqueue(30);
-        artworksTotal.Enqueue(100);
-        artworksTotal.Enqueue(50);
-        artworksTotal.Enqueue(120);
-        artworksTotal.Enqueue(80);
-        artworksTotal.Enqueue(25);
-
-
-        for(int x = 0; x < artists.Length; x++)
+        public string Name { get; set; }
+        public Dictionary <string, Artwork> Artworks { get; set; }
+        public Artist(string name)
         {
-            Console.WriteLine(artists[x]);
+            Name = name;
+            Artworks = new Dictionary<string, Artwork>();
         }
+
+        public void AddArtwork(string title, string summary)
+        {
+            Artworks[title] = new Artwork { Title = title, Summary = summary };
+        }
+    }
+
+
+    class Renaissance
+    {
+        private Dictionary<string, Artist> artists;
+        public Renaissance()
+        {
+            artists = new Dictionary<string, Artist> ();
+        }
+
+        public void AddArtist(string name)
+        {
+            // check whether there is any existing name similar to the input
+            if(!artists.ContainsKey(name))
+            {
+                artists[name] = new Artist(name);
+                Console.WriteLine($"Successfully added {name}");
+            }
+            else
+            {
+                Console.WriteLine($"{name} already exists");
+            }
+        }
+
+        public void AddArtwork(string artistName, string title, string summary)
+        {
+            if(artists.TryGetValue(artistName, out Artist artist))
+            {
+                artist.AddArtwork(title, summary);
+                Console.WriteLine($"Successfully added the '{title}' by {artistName}");
+            }
+            else
+            {
+                Console.WriteLine($"Could not find {artist}");
+            }
+
+        }
+        public void ShowGallery()
+        {
+            Console.WriteLine("Renaissance Art Gallery");
+            foreach(var person in artists)
+            {
+                Console.WriteLine($"Artist: {person.Value.Name}");
+                foreach(var piece in person.Value.Artworks)
+                {
+                    Console.WriteLine($"{piece.Key}\n {piece.Value.Summary}");
+                }
+            }
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Renaissance gallery = new Renaissance();
+            gallery.AddArtist("Leonardo da Vinci");
+            gallery.AddArtist("Sandro Botticelli");
+            gallery.AddArtist("Michelangelo");
+
+            gallery.AddArtwork("Leonardo da Vinci", "Mona Lisa", "bears strong resemblance to the Virgin Mary - who was seen as an ideal for womanhood");
+
+            gallery.ShowGallery();
+
+        }
+    // public static void Main(string[] args)
+    // {
+
+    //     string[] artists = {
+    //         "Leonardo Da Vinci",
+    //         "Michelangelo",
+    //         "Raphael",
+    //         "Donatello",
+    //         "Titian",
+    //         "Sandro Botticelli",
+    //         "El Greco",
+    //         "Caravaggio",
+    //         "Hieronymus Bosch",
+    //     };
+
+    //     Queue <int> artworksTotal = new Queue<int>();
+    //     artworksTotal.Enqueue(20);
+    //     artworksTotal.Enqueue(40);
+    //     artworksTotal.Enqueue(60);
+    //     artworksTotal.Enqueue(30);
+    //     artworksTotal.Enqueue(100);
+    //     artworksTotal.Enqueue(50);
+    //     artworksTotal.Enqueue(120);
+    //     artworksTotal.Enqueue(80);
+    //     artworksTotal.Enqueue(25);
+
+
+    //     for(int x = 0; x < artists.Length; x++)
+    //     {
+    //         Console.WriteLine(artists[x]);
+    //     }
 
         // Dictionary <string, int> renaissance = new Dictionary <string, int> ();
         // renaissance.Add("Leonardo", 20);
